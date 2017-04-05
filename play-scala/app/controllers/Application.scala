d29229df1f846d6daa209c3e48071f9543649a38
johannventer2@gmail.com <rand0m>
2017-04-05 15:55:27 +0200

    Changes made on 5 april

--- play-scala/app/controllers/Application.scala
+++ play-scala/app/controllers/Application.scala
@@ -7,6 +7,7 @@ import play.api.data.Form
 import play.api.data.Forms._
 import play.api.mvc._
 import play.api.data.validation
+import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
 import play.api.i18n.Messages
 
 
@@ -15,14 +16,6 @@ case class UserData(name: String, age: Int, contactNumber: String, email: String
 
 @Singleton
 class Application @Inject() extends Controller {
-
-  /**
-    * Create an Action to render an HTML page with a welcome message.
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-
   val userForm = Form(
     mapping(
       "name" -> nonEmptyText,
@@ -32,26 +25,37 @@ class Application @Inject() extends Controller {
     )(UserData.apply)(UserData.unapply)
   )
 
+  val nameConstraint: Constraint[String] = Constraint("constraints.namecheck")( {
+    input =>
+      val errors = input match {
+        case x: String if x.length>32  => Seq(ValidationError("Name must be shorter than 32 characters"))
+        case x: String if x.forall(_.isDigit) => Seq(ValidationError("A name cannot contain numbers"))
+        case _ => Nil
+      }
+      if(errors.isEmpty) {
+        Valid
+      } else {
+        Invalid(errors)
+      }
+  })
+
   def userPostSuccess() = Action {
     Ok(views.html.success())
   }
+
+//  def userPostFailure(data: Option[UserData] = None) = Action {
+//    BadRequest(views.html.badPost(data.getOrElse(None)))
+//  }
 
   def getUserPost() = Action { implicit request =>
     Ok(views.html.testForm(userForm, false, None))
   }
 
   def userPost = Action { implicit request =>
-//    val form = userForm.bindFromRequest().get
-//    val name = form.name
-//    val age = form.age
-//    val contactNumber = form.contactNumber
-//    val email = form.email
-//
-//     Ok(name + " : " + age + " : " + contactNumber + " : " + email)
-
     userForm.bindFromRequest.fold(
       hasErrors => {
-        BadRequest(views.html.testForm(hasErrors, false, None))
+        BadRequest(views.html.testForm(hasErrors, false, hasErrors.value))
+//        BadRequest(views.html.userPostFailure(hasErrors.value))
       }, userData => {
         //    val form = userForm.bindFromRequest().get
         val name = userData.name
@@ -60,10 +64,9 @@ class Application @Inject() extends Controller {
         val email = userData.email
         // @* Redirect(routes.Application.userPostSuccess()) *@
         //Ok(routes.Application.getUserPost(/*"Data gathered successfully!"*/))
-        Ok(views.html.testForm(userForm, true, Some(userData.name)))
+        Ok(views.html.testForm(userForm, true, Some(userData)))
         //Redirect(routes.Application.getUserPost()).flashing("success" -> "Data passed successfully!")
       }
     )
-    //Ok(views.html.testForm(userForm))
   }
 }
---
+++ play-scala/public/javascripts/myScript.coffee
@@ -0,0 +1,4 @@
+$(document).ready ->
+  $('#myForm').submit ->
+    console.log("test")
+    false
